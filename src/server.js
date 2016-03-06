41e03962ceed0e70f4c2de65429efc1199e8f235
Jake Ben-Tovim <jacobbentovim@gmail.com>
2016-03-06 00:40:14 -0500

    Huge graphics and gameplay update
    Users can now connect to each other in 1v1 games. They can see the other
    user update in real time. There is a puck which they can interact with -
    it can be hit by each player, and they can score goals for points, which
    each user is notified of. There are now graphics for each user's paddle
    and the background, as well as a background for the puck.

--- src/server.js
+++ src/server.js
@@ -1,35 +1,19 @@
 // REQUIRES
 // Load in web and file system requires, and socket.io
 var http = require("http");				// web server
-var fs = require("fs");					// file system
 var socketio = require("socket.io");	// socket.io
+var router = require("./router.js");
 var GameManager = require("./GameManager.js"); // loads in GameManager class
 
 // Attempt to use Node"s global port, otherwise use 3000
-var port = process.env.PORT || process.env.NODE_PORT || 3000;
-
-// Read the client HTML page into memory
-var index = fs.readFileSync(__dirname + "/../client/client.html");
+var PORT = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // The current room number to create - increments when a new match is created
 var curRoomNum = 1;
 
-/* onRequest 
-	desc: our callback function for when our server receives a request.
-	returns: index - our chat page
-*/
-function onRequest(request, response) {
-	// send an "ok", tell them we"re returning HTML
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write(index);
-	
-	// close the response stream
-	response.end();
-}
-
 // Start server
-var app = http.createServer(onRequest).listen(port);
-console.log("HTTP server started, listening on port " + port);
+var app = http.createServer(router).listen(PORT);
+console.log("HTTP server started, listening on port " + PORT);
 
 
 // WEBSOCKETS
@@ -57,54 +41,18 @@ function createGame() {
 	// increment room number so no users can join this room again
 	++curRoomNum;
 	
-	// add the two users to the next room in the cycle - they"re alone, ready for their match!
+	// add the two users to the next room in the cycle - they're alone, ready for their match!
 	userQueue[0].roomName = roomName;
 	userQueue[1].roomName = roomName;
 	userQueue[0].join(roomName);
 	userQueue[1].join(roomName);
-	
-	/* update player 1's info:
-		- tell them player 2's name
-		- tell them their side
-	*/
-	userQueue[0].emit(
-		"updateInfo",
-		{
-			object: "otherUser",
-			username: userQueue[1].name,
-			side: 1
-		});
-	userQueue[0].emit("updateInfo", { object: "user", side: 0 });
-	
-	/* update player 2's info:
-		- tell them player 1's name
-		- tell them their side
-	*/
-	userQueue[1].emit(
-		"updateInfo",
-		{
-			object: "otherUser",
-			username: userQueue[0].name,
-			side: 0
-		});
-	userQueue[1].emit("updateInfo", { object: "user", side: 1 });
 	
 	// create the new game instance
 	var newGame = new GameManager(roomName, io, userQueue[0], userQueue[1]);
 	currentGames.push(newGame);
 	
 	// clear those two users from the queue
-	delete userQueue[0];
-	delete userQueue[1];
-}
-
-/* loop
-	desc: main server loop, loops through currentGames and updates all of the GameManagers
-*/
-function loop() {
-	for (var i = 0; i < currentGames.length; ++i) {
-		currentGames[i].update();
-	}
+	userQueue.splice(0, 2);
 }
 
 // User joined - emitted after a socket is completed and processed
@@ -120,7 +68,7 @@ var onJoined = function(socket) {
 		
 		// store socket"s username on the socket for future use
 		socket.name = data.name;
-		
+			
 		// store the user in the database for future reference
 		users[data.name] = socket.name;
 		
@@ -130,18 +78,13 @@ var onJoined = function(socket) {
 		// notifies the user that they"re waiting for another connection
 		socket.emit("msg", { msg: "Searching for another user to play with..." });
 		
+		// tell the client the registration went through
+		socket.emit("joinSuccess");
+		
 		// attempt to create a new game room if we have two users in the queue
 		if (userQueue.length >= 2) {	
 			createGame();
 		}
-	});
-};
-
-// User update - emitted by each socket each tick, data is sent back to other user
-var onUpdate = function(socket) {
-	
-	socket.on("update", function(data) {
-		socket.broadcast.to(socket.roomName).emit("updateInfo", { object: "otherUser", pos: data.pos });
 	});
 };
 
@@ -162,9 +105,7 @@ var onDisconnect = function(socket) {
 io.sockets.on("connection", function(socket) {
 	
 	onJoined(socket);
-	onUpdate(socket);
 	onDisconnect(socket);
 });
 
 console.log("Websocket server started");
-setInterval(loop, 1);
